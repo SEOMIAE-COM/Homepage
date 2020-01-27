f432a579b74eb0aba8786a5ba10b0a57d9e59e63
Boyoung Kim <boyoung@vanary.com>
2020-01-27 17:23:15 +0900

    Add author introduction page

--- scripts/main.js
+++ scripts/main.js
@@ -17,6 +17,21 @@ Assign value to pre-defined variables
 */
 setupUI = function () {
     console.log("UI setup");
+    
+    // highlight current page link
+    const pagePaths = ["/kr/", "/kr/stories/"]
+    const pathname = location.pathname;
+
+    const currentPageIndex = pagePaths.indexOf(pathname);
+
+    if (currentPageIndex == -1) {
+        // DO NOTHING
+        console.log("page - unidentified pathname");
+    }
+    else {
+        const listItems = document.querySelectorAll(".site_navigation_listItem");
+        listItems[currentPageIndex].classList.add("selected");
+    }
 }
 
 /*
